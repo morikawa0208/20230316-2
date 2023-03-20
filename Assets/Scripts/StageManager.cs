6c5d90f42f3eab18c09cc54f28a12a7b320b5550
morikawa0208 <m.46mn.0208@icloud.com>
2023-03-20 18:03:41 +0900

    a

--- Assets/Scripts/StageManager.cs
+++ Assets/Scripts/StageManager.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class StageManager : MonoBehaviour
 {
-    //ステージ数の管理、増減、ステージ移動後の敵生成を行うスクリプト
     public static StageManager Instance { get; private set; }
 
     public int currentStage = 1;
@@ -13,14 +12,14 @@ public class StageManager : MonoBehaviour
     [SerializeField] private int maxEnemyCount = 3;
 
     [SerializeField] private float gridSpacing = 0.3f;
+    private StageController stageController;
     [SerializeField] private List<EnemyList> enemyLists = new List<EnemyList>();
-
-    [SerializeField] private List<int> startStages = new List<int>();
-    [SerializeField] private List<int> endStages = new List<int>();
 
     [System.Serializable]
     public class EnemyList
     {
+        public int startStage;
+        public int endStage;
         public string name;
         public List<GameObject> enemyPrefabs = new List<GameObject>();
         public List<float> spawnProbabilities = new List<float>();
@@ -35,6 +34,7 @@ public class StageManager : MonoBehaviour
         }
 
         Instance = this;
+        stageController = FindObjectOfType<StageController>();
     }
 
     public void ChangeStage(int stage)
@@ -44,89 +44,59 @@ public class StageManager : MonoBehaviour
 
         if (enemyLists.Count == 0) return;
 
-        // 現在のステージに対応する敵リストを取得
         var enemyList = GetEnemyListByCurrentStage();
+        if (enemyList == null) return;
+
         Debug.Log(enemyList.name);
 
-        // 既存の敵オブジェクトを破棄
         var enemies = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (var enemy in enemies)
         {
             Destroy(enemy);
         }
 
-        // 生成位置を格納するリストを作成
-        var positions = new List<Vector2>();
+        var positions = new HashSet<Vector2>();
 
-        // 抽選された生成数だけ敵を生成
-        var enemyCount = Random.Range(minEnemyCount, maxEnemyCount + 1);
+        int enemyCount = Random.Range(minEnemyCount, maxEnemyCount + 1);
         for (int i = 0; i < enemyCount; i++)
         {
-            // 敵を生成する位置を計算
-            var x = Random.Range(-2.4f, 2.4f);
-            var y = Random.Range(1.9f, 2.5f);
-            x = Mathf.Round(x / gridSpacing) * gridSpacing;
-            y = Mathf.Round(y / gridSpacing) * gridSpacing;
-
-            
-            // すでにその位置に敵がいる場合は、別の位置を計算する
-            if (positions.Contains(new Vector2(x, y))) {
-                i--;
-                continue;
-            }
-            positions.Add(new Vector2(x, y));
-
-
-            // 敵をランダムに生成
-            var randomValue = Random.Range(0f, enemyList.spawnProbabilities.Sum());
-            var spawnProbabilitySum = 0f;
-            for (int j = 0; j < enemyList.enemyPrefabs.Count; j++)
+            Vector2 position;
+            do
             {
-                spawnProbabilitySum += enemyList.spawnProbabilities[j];
-                if (randomValue <= spawnProbabilitySum)
-                {
-                    var enemyPrefab = enemyList.enemyPrefabs[j];
-                    Instantiate(enemyPrefab, new Vector3(x, y, 0), Quaternion.identity);
-                    break;
-                }
+                float x = Mathf.Round(Random.Range(-2.4f, 2.4f) / gridSpacing) * gridSpacing;
+                float y = Mathf.Round(Random.Range(1.9f, 2.5f) / gridSpacing) * gridSpacing;
+                position = new Vector2(x, y);
+            } while (positions.Contains(position));
+            positions.Add(position);
+
+            SpawnEnemyAtPosition(enemyList, position);
+        }
+    }
+
+    private void SpawnEnemyAtPosition(EnemyList enemyList, Vector2 position)
+    {
+        float randomValue = Random.Range(0f, enemyList.spawnProbabilities.Sum());
+        float spawnProbabilitySum = 0f;
+        for (int i = 0; i < enemyList.enemyPrefabs.Count; i++)
+        {
+            spawnProbabilitySum += enemyList.spawnProbabilities[i];
+            if (randomValue <= spawnProbabilitySum)
+            {
+                GameObject enemyPrefab = enemyList.enemyPrefabs[i];
+                Quaternion rotation = stageController.isRight ? Quaternion.Euler(0f, 180f, 0f) : Quaternion.identity;
+                Instantiate(enemyPrefab, (Vector3)position, rotation);
+                break;
             }
         }
     }
 
     private EnemyList GetEnemyListByCurrentStage()
     {
-        if (currentStage < startStages[0] || currentStage > endStages[endStages.Count - 1]) return null;
-
-        for (int i = 0; i < startStages.Count; i++)
-        {
-            if (currentStage >= startStages[i] && currentStage <= endStages[i])
-            {
-                return enemyLists[i];
-            }
-        }
-
-        return null;
+        return enemyLists.FirstOrDefault(enemyList => currentStage >= enemyList.startStage && currentStage <= enemyList.endStage);
     }
 
     private void Start()
     {
-        currentStage = GetCurrentStage();
+        currentStage = Mathf.Max(1, currentStage);
     }
-
-    private int GetCurrentStage()
-    {
-        if (currentStage < 1) return startStages[0];
-
-        for (int i = 0; i < startStages.Count; i++)
-        {
-            if (currentStage >= startStages[i] && currentStage <= endStages[i])
-            {
-                return currentStage - startStages[i] + (i * (endStages[i] - startStages[i] + 1));
-            }
-        }
-
-        return endStages[endStages.Count - 1] + (currentStage - endStages[endStages.Count - 1]) * (endStages[endStages.Count - 1] - startStages[startStages.Count - 1] + 1);
-    }
-
-
 }
